d4126270655bb4e4650ca6bd848ed3e3bb277902
moonlay123 <56362434+moonlay123@users.noreply.github.com>
2022-09-23 22:36:41 +0300

    lalala

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int a1=in.nextInt();
+        System.out.println("a + a1 = " + (a+a1));
+    }
+}
